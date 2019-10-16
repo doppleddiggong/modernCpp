552999a31181b4d5b6e8c1e69c966694e09244af
Bae Jubaeck <kuberin_b@naver.com>
2019-10-16 18:36:42 +0900

    포인터 테스트 코드 추가

--- algorithmus/algorithmus/PointerTest.cpp
+++ algorithmus/algorithmus/PointerTest.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "header.h"
 #include "PointerTest.h"
 
@@ -8,4 +8,17 @@ PointerTest::~PointerTest() {}
 void PointerTest::init()
 {
 	printf("PointerTest::init()\n");
+
+	int a = 3;
+	int *p = &a;
+	int *pNull = NULL;
+
+	printf("&P의 주소 : %d\n", &p);
+	printf("&a의 주소 : %d\n", &a);
+
+	printf("*p의 값 : %d\n", *p);
+	printf("(*(&a))의 값 : %d\n", (*(&a)) );
+
+	printf("pNull의 값 : %d\n", pNull );
+	printf("&pNull의 값 : %d\n", &pNull);
 }
